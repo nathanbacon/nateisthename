b6f7fc518ef3d9970caa236f1f5221714a76b85c
Nathan Gelman <nathanjlg@gmail.com>
2023-02-20 20:06:02 -0800

    fixes

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
 import "./layout.scss";
+import "purecss/build/pure-min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
---
+++ frontend/src/pages/inquire.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const InquirePage = () => {
+  return (
+    <Layout>
+      <p>Hello, world!</p>
+    </Layout>
+  );
+};
+
+export default InquirePage;
