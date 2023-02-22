5704c680ddd13b1410e608c14bcf27f8fd140fb7
Nathan Gelman <nathanjlg@gmail.com>
2023-02-22 10:24:30 -0800

    function auth change

--- functions/NateFunctions/HelloWorld.cs
+++ functions/NateFunctions/HelloWorld.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace NateFunctions
 {
@@ -14,8 +15,8 @@ namespace NateFunctions
   {
     [FunctionName("HelloWorld")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.User, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log, ClaimsPrincipal principal)
     {
       log.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -23,8 +24,8 @@ namespace NateFunctions
 
       string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
       dynamic data = JsonConvert.DeserializeObject(requestBody);
-      name = name ?? data?.name;
 
+      name = principal.Identity.Name ?? "None";
       string responseMessage = string.IsNullOrEmpty(name)
           ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
           : $"Hello, {name}. This HTTP triggered function executed successfully.";
